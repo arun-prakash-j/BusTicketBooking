40301bc15f701d9e11263d6a96cf2bbc818ab0ed
Arun Prakash J <arun.prakash@idp.com>
2023-09-21 11:38:45 +0530

    Added all the required components

--- bus-ticket-booking/src/app/app-routing.module.ts
+++ bus-ticket-booking/src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerLoginComponent } from './customer-login/customer-login.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminInterfaceComponent } from './admin-interface/admin-interface.component';
+import { BusListComponent } from './bus-list/bus-list.component';
+import { SeatSelectionComponent } from './seat-selection/seat-selection.component';
+import { PassengerInfoComponent } from './passenger-info/passenger-info.component';
+import { BookingSummaryComponent } from './booking-summary/booking-summary.component';
 
 const routes: Routes = [
   { path: 'customer-login', component: CustomerLoginComponent },
   { path: 'admin-login', component: AdminLoginComponent },
-  { path: '', redirectTo: '/customer-login', pathMatch: 'full' }, // Default route
+  { path: 'admin', component: AdminInterfaceComponent },
+  { path: 'buses', component: BusListComponent },
+  { path: 'buses/:busId/seats', component: SeatSelectionComponent },
+  {
+    path: 'buses/:busId/seats/:seatId/passenger-info',
+    component: PassengerInfoComponent,
+  },
+  { path: 'booking-summary', component: BookingSummaryComponent },
+  // Add more routes as needed
+  { path: '', redirectTo: '/customer-login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- bus-ticket-booking/src/app/app.module.ts
+++ bus-ticket-booking/src/app/app.module.ts
@@ -6,9 +6,14 @@ import { CustomerLoginComponent } from './customer-login/customer-login.componen
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
+import { AdminInterfaceComponent } from './admin-interface/admin-interface.component';
+import { BusListComponent } from './bus-list/bus-list.component';
+import { SeatSelectionComponent } from './seat-selection/seat-selection.component';
+import { PassengerInfoComponent } from './passenger-info/passenger-info.component';
+import { BookingSummaryComponent } from './booking-summary/booking-summary.component';
 
 @NgModule({
-  declarations: [AppComponent, CustomerLoginComponent, AdminLoginComponent],
+  declarations: [AppComponent, CustomerLoginComponent, AdminLoginComponent, AdminInterfaceComponent, BusListComponent, SeatSelectionComponent, PassengerInfoComponent, BookingSummaryComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
